087192078f7f3e245febf967df92b32b1953d56a
Anas Khan <anask@ip-172-31-32-200.us-west-2.compute.internal>
2017-02-25 21:49:59 +0000

    ...

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var bodyParser=require('body-parser');
 
-app.set('port',(process.env.PORT||5000));
+app.set('port',5000);
 
 app.use(express.static(__dirname+'/public'));
 app.use(bodyParser.json());
@@ -12,6 +12,7 @@ app.set('views',__dirname+'/views');
 app.set('view engine','ejs');
 
 app.get('/',function(request,response){
+	console.log("GET 200");
 	response.render("Starfield.ejs");
 });
 
