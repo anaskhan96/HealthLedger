bb79c9fb54d1aaa5e60c44df81bbfa7be0a8d8a8
Anas Khan <anas.khan96@outlook.com>
2017-02-26 01:25:01 +0530

    initial

---
+++ index.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var app = express();
+var bodyParser=require('body-parser');
+
+app.set('port',(process.env.PORT||5000));
+
+app.use(express.static(__dirname+'/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.set('views',__dirname+'/views');
+app.set('view engine','ejs');
+
+app.get('/',function(request,response){
+	response.render("Starfield.ejs");
+});
+
+app.get('/ledger',function(request,response){
+	response.send("Work to do over here...");
+});
+
+app.listen(app.get('port'),function(){
+	console.log("Node app is running on localhost:",app.get('port'));
+});
