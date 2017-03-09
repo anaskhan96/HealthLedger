cba1291120b2394cc0f3d4ff45a82f8f7f4b5ddd
Anas Khan <anas.khan96@outlook.com>
2017-03-09 17:03:08 +0530

    stuff

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var app = express();
 var bodyParser=require('body-parser');
+var mongodb = require('mongodb');
+var mc = mongodb.MongoClient;
+var url='mongodb://localhost:27017/test';
+mc.connect(url,function(err,db){
+	if(!err){
+		console.log("Connected!");
+	}
+	collection=db.collection('sample');
+});
 
 app.set('port',5000);
 
@@ -13,6 +22,13 @@ app.set('view engine','ejs');
 
 app.get('/ledger',function(request,response){
 	response.render("ledger_home.ejs");
+});
+
+app.post('/ledger',function(request,response){
+	var test=request.body;
+	collection.insert({"id":1,"name":test['msg']});
+	console.log("Inserted!");
+	
 });
 
 app.listen(app.get('port'),function(){
