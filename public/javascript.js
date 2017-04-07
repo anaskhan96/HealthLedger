69d72fea8ab869e32a7ed40d5354998c6fcce5ee
AnvithShetty10 <anvithshetty97@gmail.com>
2017-04-08 00:53:33 +0530

    doctor page added

--- public/javascript.js
+++ public/javascript.js
@@ -208,16 +208,3 @@ function readMore(event) {
 		d.appendChild(e);
 	}
 }
-
-(function ($) {
-	$(window).scroll(function () {
-
-		$(window).scroll(function () {
-			space = $(window).innerHeight() - $('.fab').offsetTop + $('.fab').offsetHeight;
-			if (space < 200) {
-				$('.fab').css('margin-bottom', '150px');
-			}
-		})
-	});
-
-})(jQuery);
